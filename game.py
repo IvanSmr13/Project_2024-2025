d2f7e4e3f34db97d750a60f752592900de8582b1
Ivan <smirnov_ivan_n@mail.ru>
2024-12-08 18:37:08 +0300

    Update game.py

--- game.py
+++ game.py
@@ -10,3 +10,9 @@ WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
 RED = (255, 0, 0)
 GREEN = (0, 255, 0)
+
+class Player(pygame.sprite.Sprite):
+    def __init__(self):
+        super().__init__() # замена pygame.sprite.Sprite.__init__(self)
+        self.rect = self.image.get_rect(center=(WIDTH // 2, HEIGHT - 50))
+        self.image = pygame.image.load("tank.png")
