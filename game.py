50c7b5737db89a57aa72a936ba18c5dd485481e7
Ivan <smirnov_ivan_n@mail.ru>
2024-12-08 18:37:46 +0300

    Update game.py

--- game.py
+++ game.py
@@ -5,6 +5,10 @@ WIDTH = 800
 HEIGHT = 600
 FPS = 30
 
+PLAYER_H_SPEED = 5
+PLAYER_V_SPEED = 3
+
+
 # Установка цветов
 WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
@@ -16,3 +20,14 @@ class Player(pygame.sprite.Sprite):
         super().__init__() # замена pygame.sprite.Sprite.__init__(self)
         self.rect = self.image.get_rect(center=(WIDTH // 2, HEIGHT - 50))
         self.image = pygame.image.load("tank.png")
+
+    def update(self):
+        keys = pygame.key.get_pressed() # метод, который получает состояние всех кнопок клавиатуры
+        if keys[pygame.K_LEFT] and self.rect.left > 0:
+            self.rect.x -= PLAYER_H_SPEED
+        if keys[pygame.K_RIGHT] and self.rect.right < WIDTH:
+            self.rect.x += PLAYER_H_SPEED
+        if keys[pygame.K_UP] and self.rect.top < HEIGHT:
+            self.rect.y += PLAYER_V_SPEED
+        if keys[pygame.K_DOWN] and self.rect.bottom > 0:
+            self.rect.y -= PLAYER_V_SPEED
