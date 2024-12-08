aa772f622fdbdbf898eb56dd3b75c7baf176cfaf
Ivan <smirnov_ivan_n@mail.ru>
2024-12-08 19:39:00 +0300

    Update game.py

--- game.py
+++ game.py
@@ -21,12 +21,15 @@ GREEN = (0, 255, 0)
 # Класс "Игрок"
 class Player(pygame.sprite.Sprite):
     def __init__(self):
-        super().__init__() # замена pygame.sprite.Sprite.__init__(self)
-        self.rect = self.image.get_rect(center=(WIDTH // 2, HEIGHT - 50))
+        super().__init__() # Замена pygame.sprite.Sprite.__init__(self)
         self.image = pygame.image.load("tank.png")
+        self.rect = self.image.get_rect(center=(WIDTH // 2, HEIGHT - 50))
+    
+        
+        
 
     def update(self):
-        keys = pygame.key.get_pressed() # метод, который получает состояние всех кнопок клавиатуры
+        keys = pygame.key.get_pressed() # Метод, который получает состояние всех кнопок клавиатуры
         if keys[pygame.K_LEFT] and self.rect.left > 0:
             self.rect.x -= PLAYER_H_SPEED
         if keys[pygame.K_RIGHT] and self.rect.right < WIDTH:
@@ -51,15 +54,46 @@ class Enemy(pygame.sprite.Sprite):
         self.rect.x = random.randint(0, WIDTH)
         self.rect.y = random.randint(-100, -40)
 
-
+# Инициализация игры
 pygame.init()
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("World of tanks")
 clock = pygame.time.Clock()
 all_sprites = pygame.sprite.Group()
+enemies = pygame.sprite.Group()
 player = Player()
-all_sprites.add(player)
-        
+all_sprites.add(Player)
 
-        
+
+# Фон игры
+background_image = pygame.image.load('for_game.jpg')
+while True:
+    screen.blit(background_image, (0, 0))
+    pygame.display.update()
+    clock.tick(60)
+
+
+# Сама игра
+running = True
+game_over = False
+
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    if not game_over:
+        all_sprites.update()
+
+
+    if pygame.sprite.spritecollide(player, enemies, False):
+            game_over = True
+
+all_sprites.draw(screen)
+
+
+
+
+
+          
 pygame.quit
